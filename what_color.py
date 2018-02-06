252bf271c4397e4f7817611d037f80821ea13deb
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2018-02-06 12:57:54 -0500

    a way to quantize the colors and measure their percentage

--- what_color.py
+++ what_color.py
@@ -4,22 +4,47 @@ import cv2
 
 from PIL import Image
 from color_utils import get_palette
+from scipy.spatial.distance import cdist
 
 
 def pil_to_cv2(img):
     img = cv2.cvtColor(np.array(img), cv2.COLOR_BGR2RGB)
     return img / 255
 
-def display_palette(palette):
+def display_palette(palette, percentages=None):
     num_colors = len(palette)
     colors = np.zeros([32, 32*num_colors, 3], dtype=np.float32)
     for color in range(num_colors):
         colors[0:32, 32*color:32*(color+1)] = palette[color]
+        if percentages is not None:
+            txt = str(percentages[color])
+            txt_color = list(map(lambda x: 255 - x, palette[color]))
+            cv2.putText(colors, txt, (2+32*color,16), cv2.FONT_HERSHEY_SIMPLEX, 0.4, txt_color)
     return colors
 
 def get_B_img(img):
     w, h = img.size
     return img.crop((w/2, 0, w, h))
+
+def palettify(img, palette):
+    img = np.array(img)
+    # we add white because we want to ignore it
+    palette = np.array(palette + [[255, 255, 255]])
+
+    closest_points = cdist(img.reshape((-1, 3)), palette)
+    palette_idx = np.argmin(closest_points, axis=1).astype(np.int8)
+    palettified = palette[palette_idx]
+    return palettified.reshape(img.shape).astype(np.uint8)
+
+def get_palette_percentages(img, palette):
+    img = np.array(img)
+    # we add white because we want to ignore it
+    palette = np.array(palette + [[255, 255, 255]])
+
+    closest_points = cdist(img.reshape((-1, 3)), palette)
+    palette_idx = np.argmin(closest_points, axis=1).astype(np.int8)
+    counts = np.unique(palette_idx, return_counts=True)[1][:-1] # remove white
+    return counts / sum(counts)
 
 if __name__ == '__main__':
     base_path = 'datasets/edges2shoes/train/'
@@ -27,7 +52,18 @@ if __name__ == '__main__':
     for path in os.listdir(base_path):
         img = get_B_img(Image.open(base_path + path))
         palette = get_palette(img, color_count=6)
+        palette_percentages = get_palette_percentages(img, palette)
+
+        cv2.namedWindow(path)
+        cv2.moveWindow(path, 30, 175)
+        cv2.namedWindow('{} palette'.format(path))
+        cv2.moveWindow('{} palette'.format(path), 30, 30)
+        cv2.namedWindow('{} palettified'.format(path))
+        cv2.moveWindow('{} palettified'.format(path), 30, 500)
+
         cv2.imshow(path, pil_to_cv2(img))
-        cv2.imshow('{} palette'.format(path), pil_to_cv2(display_palette(palette)))
+        cv2.imshow('{} palette'.format(path), pil_to_cv2(display_palette(palette, percentages=palette_percentages)))
+        cv2.imshow('{} palettified'.format(path), pil_to_cv2(palettify(img, palette)))
+
         cv2.waitKey(0)
         cv2.destroyAllWindows()
