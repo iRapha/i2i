7e73e5781649a8721d6a54a26c4cd45769b9763a
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2018-02-20 13:02:22 -0500

    not sure why this restarted but it stopped early?

--- what_color.py
+++ what_color.py
@@ -8,10 +8,12 @@ from scipy.spatial.distance import cdist
 
 
 def pil_to_cv2(img):
+    """Converts a numpy image into a cv2-display-suitable representation."""
     img = cv2.cvtColor(np.array(img), cv2.COLOR_BGR2RGB)
     return img / 255
 
 def display_palette(palette, percentages=None):
+    """Returns an image representation of the palette for display."""
     num_colors = len(palette)
     colors = np.zeros([32, 32*num_colors, 3], dtype=np.float32)
     for color in range(num_colors):
@@ -23,10 +25,12 @@ def display_palette(palette, percentages=None):
     return colors
 
 def get_B_img(img):
+    """Returns the right half of an image."""
     w, h = img.size
     return img.crop((w/2, 0, w, h))
 
 def palettify(img, palette):
+    """Returns an image w/ each pixel as its nearest neighbor in the palette."""
     img = np.array(img)
     # we add white because we want to ignore it
     palette = np.array(palette + [[255, 255, 255]])
@@ -37,6 +41,7 @@ def palettify(img, palette):
     return palettified.reshape(img.shape).astype(np.uint8)
 
 def get_palette_percentages(img, palette):
+    """Returns the % of pixels in img that are NNs of each color in palette."""
     img = np.array(img)
     # we add white because we want to ignore it
     palette = np.array(palette + [[255, 255, 255]])
@@ -46,9 +51,8 @@ def get_palette_percentages(img, palette):
     counts = np.unique(palette_idx, return_counts=True)[1][:-1] # remove white
     return counts / sum(counts)
 
-if __name__ == '__main__':
-    base_path = 'datasets/edges2shoes/train/'
-
+def visualize_all(base_path):
+    """For each image, computes the palette and displays palettified image."""
     for path in os.listdir(base_path):
         img = get_B_img(Image.open(base_path + path))
         palette = get_palette(img, color_count=6)
@@ -67,3 +71,19 @@ if __name__ == '__main__':
 
         cv2.waitKey(0)
         cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    base_path = 'datasets/edges2shoes/train/'
+    all_colors_file = 'shoes_colors.csv'
+
+    num_imgs = len(os.listdir(base_path))
+
+    with open(all_colors_file, 'w+') as f:
+        for i, path in enumerate(os.listdir(base_path)):
+            if i % 10 == 0: print('{}/{}'.format(i, num_imgs), end='\r')
+
+            img = get_B_img(Image.open(base_path + path))
+            palette = get_palette(img, color_count=6)
+            palette_percentages = get_palette_percentages(img, palette)
+            for color, weight in zip(palette, palette_percentages):
+                f.write('{}, {}, {}, {}\n'.format(*color, weight))
