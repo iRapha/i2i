8f37d84470b8eec64835fb82fdfad642be5a80c6
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2018-02-23 18:19:00 -0500

    rename palette utils and script file

--- palette.py
+++ palette.py
@@ -5,12 +5,8 @@ import cv2
 from PIL import Image
 from color_utils import get_palette
 from scipy.spatial.distance import cdist
+from img_utils import pil_to_cv2, get_B_img
 
-
-def pil_to_cv2(img):
-    """Converts a numpy image into a cv2-display-suitable representation."""
-    img = cv2.cvtColor(np.array(img), cv2.COLOR_BGR2RGB)
-    return img / 255
 
 def display_palette(palette, percentages=None):
     """Returns an image representation of the palette for display."""
@@ -23,11 +19,6 @@ def display_palette(palette, percentages=None):
             txt_color = list(map(lambda x: 255 - x, palette[color]))
             cv2.putText(colors, txt, (2+32*color,16), cv2.FONT_HERSHEY_SIMPLEX, 0.4, txt_color)
     return colors
-
-def get_B_img(img):
-    """Returns the right half of an image."""
-    w, h = img.size
-    return img.crop((w/2, 0, w, h))
 
 def palettify(img, palette):
     """Returns an image w/ each pixel as its nearest neighbor in the palette."""
@@ -58,32 +49,45 @@ def visualize_all(base_path):
         palette = get_palette(img, color_count=6)
         palette_percentages = get_palette_percentages(img, palette)
 
-        cv2.namedWindow(path)
-        cv2.moveWindow(path, 30, 175)
-        cv2.namedWindow('{} palette'.format(path))
-        cv2.moveWindow('{} palette'.format(path), 30, 30)
-        cv2.namedWindow('{} palettified'.format(path))
-        cv2.moveWindow('{} palettified'.format(path), 30, 500)
+        visualize_one(path, img, palette, palette_percentages)
 
-        cv2.imshow(path, pil_to_cv2(img))
-        cv2.imshow('{} palette'.format(path), pil_to_cv2(display_palette(palette, percentages=palette_percentages)))
-        cv2.imshow('{} palettified'.format(path), pil_to_cv2(palettify(img, palette)))
+def visualize_one(path, img, palette, palette_percentages, save=False):
+    og_img = pil_to_cv2(img)
+    palette_img = pil_to_cv2(display_palette(palette, percentages=palette_percentages))
+    palletified_img = pil_to_cv2(palettify(img, palette))
+    if save:
+        cv2.imwrite(path + '.png', og_img)
+        cv2.imwrite('{}_palette.png'.format(path), palette_img)
+        cv2.imwrite('{}_palettified.png'.format(path), palletified_img)
+        return
+    cv2.namedWindow(path)
+    cv2.moveWindow(path, 30, 175)
+    cv2.namedWindow('{} palette'.format(path))
+    cv2.moveWindow('{} palette'.format(path), 30, 30)
+    cv2.namedWindow('{} palettified'.format(path))
+    cv2.moveWindow('{} palettified'.format(path), 30, 500)
 
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+    cv2.imshow(path, og_img)
+    cv2.imshow('{} palette'.format(path), palette_img)
+    cv2.imshow('{} palettified'.format(path), palletified_img)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    base_path = 'datasets/edges2shoes/train/'
-    all_colors_file = 'shoes_colors.csv'
+    base_path = 'datasets/edges2handbags/train/'
+    all_colors_file = 'summaries/color/handbags_train.csv'
 
     num_imgs = len(os.listdir(base_path))
 
-    with open(all_colors_file, 'w+') as f:
+    with open(all_colors_file, 'w') as f:
         for i, path in enumerate(os.listdir(base_path)):
-            if i % 10 == 0: print('{}/{}'.format(i, num_imgs), end='\r')
+            if i % 10 == 0:
+                print('{}/{}'.format(i, num_imgs), end='\r')
 
             img = get_B_img(Image.open(base_path + path))
             palette = get_palette(img, color_count=6)
             palette_percentages = get_palette_percentages(img, palette)
             for color, weight in zip(palette, palette_percentages):
                 f.write('{}, {}, {}, {}\n'.format(*color, weight))
+        print('') # new line
