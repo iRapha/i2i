1e26963c51a0de72e4d43f3ebd9987b080e44006
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2018-02-26 10:46:49 -0500

    allow import of step in sorted_sprite

--- sorted_sprite.py
+++ sorted_sprite.py
@@ -4,21 +4,6 @@ import numpy as np
 from PIL import Image
 
 
-dataset = 'handbags_train'
-
-
-# LOAD ALL PALETTE PIXELS
-with open('summaries/color/{}.csv'.format(dataset), 'r') as f:
-    csv = f.read().split('\n')
-    N = len(csv) - 1 # Number of items.
-    D = 3 # Dimensionality of the embedding.
-    all_ex = []
-    for i, ex in enumerate(csv):
-        if ex == '': continue
-        ex_split = ex.split(', ')
-        all_ex.append([int(x) for x in ex_split[0:3]])
-
-# STEP SORTING
 def step (r,g,b, repetitions=1):
     lum = np.sqrt(.241 * r + .691 * g + .068 * b)
 
@@ -30,24 +15,40 @@ def step (r,g,b, repetitions=1):
 
     return (h2, lum, v2)
 
-for reps in range(1, 17):
-    print('reps: {}'.format(reps))
-    all_ex.sort(key=lambda rgb: step(rgb[0], rgb[1], rgb[2], reps))
+if __name__ == '__main__':
+    dataset = 'handbags_train'
 
-    # VISUALIZE
-    num_ex = len(all_ex)
+    # LOAD ALL PALETTE PIXELS
+    with open('summaries/color/{}.csv'.format(dataset), 'r') as f:
+        csv = f.read().split('\n')
+        N = len(csv) - 1 # Number of items.
+        D = 3 # Dimensionality of the embedding.
+        all_ex = []
+        for i, ex in enumerate(csv):
+            if ex == '': continue
+            ex_split = ex.split(', ')
+            all_ex.append([int(x) for x in ex_split[0:3]])
 
-    px_height = 15
-    height = int(np.ceil(np.sqrt(num_ex/px_height)))
-    width = height * px_height
+    for reps in range(1, 17):
+        print('reps: {}'.format(reps))
 
-    sprite = np.zeros((height * px_height, width, 3), np.uint8)
+        # STEP SORTING
+        all_ex.sort(key=lambda rgb: step(rgb[0], rgb[1], rgb[2], reps))
 
-    for i, pixels in enumerate(all_ex):
-        x = i % width
-        y = (i // width) * px_height
+        # VISUALIZE
+        num_ex = len(all_ex)
 
-        sprite[y:y+px_height, x, :] = pixels
+        px_height = 15
+        height = int(np.ceil(np.sqrt(num_ex/px_height)))
+        width = height * px_height
 
-    im = Image.fromarray(sprite)
-    im.save('summaries/sorted/sorted_{}_{}.png'.format(dataset, reps))
+        sprite = np.zeros((height * px_height, width, 3), np.uint8)
+
+        for i, pixels in enumerate(all_ex):
+            x = i % width
+            y = (i // width) * px_height
+
+            sprite[y:y+px_height, x, :] = pixels
+
+        im = Image.fromarray(sprite)
+        im.save('summaries/sorted/sorted_{}_{}.png'.format(dataset, reps))
